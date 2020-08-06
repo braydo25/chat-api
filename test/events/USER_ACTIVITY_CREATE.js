d0c0e596275a7174467de8dcafc353017f773664
Braydon Batungbacal <braydon@tryshared.com>
2020-08-06 06:04:37 -0700

    Added event for user activity creation.

---
+++ test/events/USER_ACTIVITY_CREATE.js
@@ -0,0 +1,60 @@
+const Joi = require('@hapi/joi');
+
+/*
+ * Validator
+ */
+
+module.exports = payload => {
+  const validationResult = Joi.object({
+    event: Joi.string(),
+    data: Joi.object({
+      id: Joi.number(),
+      userId: Joi.number(),
+      createdAt: Joi.date(),
+      userFollower: Joi.object({
+        id: Joi.number(),
+        userId: Joi.number(),
+        followerUser: Joi.object({
+          id: Joi.number(),
+          username: Joi.string(),
+          name: Joi.string(),
+          lastActiveAt: Joi.date(),
+          avatarAttachment: Joi.object({
+            id: Joi.number(),
+            url: Joi.string(),
+            mimetype: Joi.string(),
+          }).allow(null),
+        }),
+      }).optional(),
+      conversationRepost: Joi.object({
+        id: Joi.number(),
+        userId: Joi.number(),
+        conversationId: Joi.number(),
+        createdAt: Joi.date(),
+        conversation: Joi.object({
+          id: Joi.number(),
+          title: Joi.string(),
+        }),
+        user: Joi.object({
+          id: Joi.number(),
+          username: Joi.string(),
+          name: Joi.string(),
+          lastActiveAt: Joi.date(),
+          avatarAttachment: Joi.object({
+            id: Joi.number(),
+            url: Joi.string(),
+            mimetype: Joi.string(),
+          }).allow(null),
+        }),
+      }).optional(),
+    }),
+  }).validate(payload, {
+    allowUnknown: true,
+    presence: 'required',
+  });
+
+  if (validationResult.error) {
+    console.dir(validationResult, { depth: null });
+    throw validationResult.error;
+  }
+};
